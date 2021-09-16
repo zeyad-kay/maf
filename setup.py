161325eec8a65b81b8214efcd88e4397bc3ad0bc
zeyadk <zeyad_khaled@outlook.com>
2021-09-16 18:28:41 +0200

    docs: update package description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='maf',
       version='0.2',
-      description='A package for manipulating maf files and mrms',
+      description='A package for maf files io',
       author='Zeyad Khaled',
       url='https://github.com/zeyad-kay/maf',
       packages=['maf'],
