6e7f826f9fabdbb6f5f922fdaf7a4a93128e2e54
zeyadk <zeyad_khaled@outlook.com>
2021-09-16 18:27:50 +0200

    build: update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='maf',
-      version='0.1',
+      version='0.2',
       description='A package for manipulating maf files and mrms',
       author='Zeyad Khaled',
-      url='https://bitbucket.org/zeyadk/mrms',
+      url='https://github.com/zeyad-kay/maf',
       packages=['maf'],
       )
