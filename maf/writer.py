5e6c81994a7be6e2aeee2910953279ff79c45acd
zeyadk <zeyad_khaled@outlook.com>
2021-09-16 18:26:03 +0200

    feat (writer): add maf writer module

---
+++ maf/writer.py
@@ -0,0 +1,28 @@
+import gzip
+
+
+class MAFWriter:
+    def __init__(self, path, compression=None):
+        self.path = path
+        self.compression_extensions = {
+            "gzip": ".gz"
+        }
+        self._parsers = {
+            "gzip": gzip.open,
+            "native": open
+        }
+        self.compression = compression
+        if self.compression:
+            self._parser = self._parsers[compression]
+            self.path += self.compression_extensions[compression]
+        else:
+            self._parser = self._parsers["native"]
+
+    def write(self, data):
+        with self._parser(self.path, "wb") as file:
+            raw_data = ""
+            for row in data:
+                raw_data = raw_data + \
+                    "".join([val + "\t" for val in row]).rstrip("\t") + "\n"
+
+            file.write(raw_data.encode())
