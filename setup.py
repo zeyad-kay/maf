1300a32b8744d2a5e15f26edfb0f535d68d04a16
zeyad-khaled <45128084+zeyad-kay@users.noreply.github.com>
2022-01-04 23:43:08 +0200

    bump t0 v1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='mafio',
-      version='1.0.0',
+      version='1.0.1',
       description='A package for maf files io',
       long_description=long_description,
       long_description_content_type="text/markdown",
